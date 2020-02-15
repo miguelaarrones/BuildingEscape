b918cade1af4c3c7de4d9539e6d83effa53ba2c8
maarrones <miguelaarrones#icloud.com>
2020-02-15 02:07:00 +0100

    Played around with pointers and fixed some settings.

--- Source/BuildingScape/WorldPosition.cpp
+++ Source/BuildingScape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,8 +20,8 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("This is a warning"));
-	
+	// FString ActorName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Object name is: %s"), *GetOwner()->GetName() );
 }
 
 
